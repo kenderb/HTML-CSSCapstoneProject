459d37ad898b77521e2e22fcd6d3d7f90337f88a
kenderb <kenderbolivar@gmail.com>
2020-10-18 13:33:46 -0500

    add description page

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -7,3 +7,10 @@ listSearchTag.forEach(element => {
   };
 });
 
+function myMap() {
+  var mapProp= {
+    center:new google.maps.LatLng(51.508742,-0.120850),
+    zoom:5,
+  };
+  var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+  }
