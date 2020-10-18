b2091a4a4e1e4cfe50798bf78f5a4d615049045e
kenderb <kenderbolivar@gmail.com>
2020-10-18 14:46:33 -0500

    finish last page

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -6,11 +6,3 @@ listSearchTag.forEach(element => {
     location.href = "./search-result.html";
   };
 });
-
-function myMap() {
-  var mapProp= {
-    center:new google.maps.LatLng(51.508742,-0.120850),
-    zoom:5,
-  };
-  var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-  }
